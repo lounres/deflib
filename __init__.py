3d34dcafcd52263964d905fe47ca8468fcb7e66d
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2019-09-20 16:11:05 +0300

    Realized DMDCL and imported it in deflib\__init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .combinatorial import binomial, gcd
 # from .graph import Graph
-from .lists import DynamicCoordList  # , MultiDimCoordList, DynamicMultiDimCoordList, MultiDimList
+from .lists import DynamicCoordList, DynamicMultiDimCoordList  # , MultiDimCoordList, MultiDimList
 from .polynomials import Polynomial, polynomial_gcd, interpolation_polynomial, NewPolynomial
 from .rings_n_fields import Rational
 from .sets import PseudoFullSet
